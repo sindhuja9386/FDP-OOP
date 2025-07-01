9263f23f6e249da3d99af1c301486b6ea5bb3228
sindhuja9386 <sindhuakbalaji@gmail.com>
2025-07-01 10:21:06 +0530

    Add files via upload

---
+++ CarTest.java
@@ -0,0 +1,27 @@
+
+
+	class Car {
+		 String model;
+		 String color;
+		 Car(String model, String color) {
+		 this.model = model;
+		 this.color = color;
+		 }
+		 void start() {
+		 System.out.println(model + " is startng.");
+		 }
+		 void stop() {
+		 System.out.println(model + " is stopping.");
+		 }
+		}
+		public class CarTest {
+		 public static void main(String[] args) {
+		 Car myCar = new Car("Toyota Corolla", "Red");
+		 System.out.println("Car Model: " + myCar.model);
+		 System.out.println("Car Color: " + myCar.color);
+		 myCar.start();
+		 myCar.stop(); 
+
+}
+}
+		
---
+++ Three.java
@@ -0,0 +1,10 @@
+
+public class Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sindhu");
+
+	}
+
+}
